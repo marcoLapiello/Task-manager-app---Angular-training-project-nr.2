25c8ed8b476804a9a7f456af060e2678979ad1bc
marcoLapiello <marcolapiello@gmail.com>
2024-11-28 10:57:15 +0100

    Header component built + some basic style added

---
+++ src/app/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    standalone: true, // only needed below Angular 19
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+}
