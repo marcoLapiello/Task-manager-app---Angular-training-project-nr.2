b5ff22713295a159f301f86995c7aa16c8de5565
marcoLapiello <marcolapiello@gmail.com>
2024-11-29 16:24:31 +0100

    content projection and pipe concepts applied

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
 import { type Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CommonModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
